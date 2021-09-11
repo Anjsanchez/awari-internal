015670599a8af646a2771567da5ce1efb6bbe4ac
anjlsanchez <anjlosanchez@gmail.com>
2021-09-11 23:48:03 +0800

    done header SOA

--- client-app/src/components/Prints/SOA/SOA.jsx
+++ client-app/src/components/Prints/SOA/SOA.jsx
@@ -1,29 +1,191 @@
 import React from "react";
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import {
+  Page,
+  Text,
+  Font,
+  Document,
+  StyleSheet,
+  Image,
+  View,
+} from "@react-pdf/renderer";
 import ReactPDF from "@react-pdf/renderer";
 import { PDFViewer } from "@react-pdf/renderer";
+
+Font.register({
+  family: "Oswald",
+  src: "https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf",
+});
+
 const styles = StyleSheet.create({
-  page: {
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
+  //TABLE
+  table: {
+    marginTop: 30,
+    display: "table",
+    width: "auto",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
   },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
+  tableRow: { margin: "auto", flexDirection: "row" },
+  tableCol: {
+    width: "25%",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+  },
+  tableCell: { margin: "auto", marginTop: 5, fontSize: 10 },
+  customerWrapper: {
+    marginTop: 5,
+    textAlign: "left",
+  },
+  //END
+  body: {
+    paddingBottom: 65,
+    paddingHorizontal: 10,
+  },
+  pageNumber: {
+    position: "absolute",
+    fontSize: 12,
+    bottom: 30,
+    left: 0,
+    right: 0,
+    textAlign: "center",
+    color: "grey",
+  },
+
+  page: { flexDirection: "column", padding: 25 },
+
+  rowResort: {
+    display: "flex",
+    justifyContent: "flex-end",
+    textAlign: "right",
+    flexDirection: "row",
+  },
+  rowCustomer: {
+    display: "flex",
+    justifyContent: "flex-start",
+    textAlign: "left",
+    flexDirection: "row",
+  },
+  resortWrapper: {
+    marginTop: 5,
+    borderRight: 1,
+    paddingRight: 15,
+    textAlign: "right",
+  },
+  rowRight: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    textAlign: "right",
+    fontSize: 13,
+  },
+  image: {
+    width: 100,
+  },
+  centerImage: {
+    alignItems: "center",
+    paddingLeft: 10,
+  },
+  titleText: {
+    textAlign: "center",
+    marginTop: 10,
+    fontSize: 25,
+    fontWeight: "bold",
   },
 });
 
 const SOAe = () => {
   return (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.section}>
-          <Text>Section #1</Text>
+    <Document title="Statement of Account">
+      <Page size="A4" style={styles.body}>
+        <View>
+          <View style={styles.rowResort}>
+            <View style={[styles.resortWrapper]}>
+              <View style={styles.rowRight}>
+                <Text>Awari Anilao Bay Resort</Text>
+              </View>
+              <View style={styles.rowRight}>
+                <Text>Brgy Solo, Mabini, Batangas Philippines</Text>
+              </View>
+              <View style={styles.rowRight}>
+                <Text>+63 9177203915/ +63 9989940866</Text>
+              </View>
+              <View style={styles.rowRight}>
+                <Text>www.anilaoawaribay.com</Text>
+              </View>
+              <View style={styles.rowRight}>
+                <Text>TIN: 008-712-086-000</Text>
+              </View>
+            </View>
+
+            <View style={styles.centerImage}>
+              <Image style={styles.image} src="/img/awari.jpg" />
+            </View>
+          </View>
         </View>
-        <View style={styles.section}>
-          <Text>Section #2</Text>
+
+        <View>
+          <Text style={styles.titleText}>Customer Statement</Text>
         </View>
+
+        <View>
+          <View style={styles.rowCustomer}>
+            <View style={[styles.customerWrapper]}>
+              <View style={styles.rowRight}>
+                <Text>Awari Anilao Bay Resort</Text>
+              </View>
+              <View style={styles.rowRight}>
+                <Text>TIN: 008-712-086-000</Text>
+              </View>
+            </View>
+
+            <View style={styles.rowRight}>
+              <Text>TIN: 008-712-086-000</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.table}>
+          {/* TableHeader */}
+          <View style={styles.tableRow}>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>Product</Text>
+            </View>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>Type</Text>
+            </View>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>Period</Text>
+            </View>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>Price</Text>
+            </View>
+          </View>
+          {/* TableContent */}
+          <View style={styles.tableRow}>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>React-PDF</Text>
+            </View>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>3 User </Text>
+            </View>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>2019-02-20 - 2020-02-19</Text>
+            </View>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>5€</Text>
+            </View>
+          </View>
+        </View>
+        <Text
+          style={styles.pageNumber}
+          render={({ pageNumber, totalPages }) =>
+            `${pageNumber} / ${totalPages}`
+          }
+          fixed
+        />
       </Page>
     </Document>
   );
